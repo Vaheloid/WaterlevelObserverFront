564b7c6e2c06b507176879c46f1b5cc752b9b85f
Vaheloid <vlad.tyupin@yandex.ru>
2026-03-27 15:44:13 +0500

    Добавлен защищенный маршрут.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import '@/index.css'
 import Login from '@/components/Login/Login.tsx'
 import MainPage from '@/components/MainPage/MainPage.tsx'
 import { Flex } from '@chakra-ui/react'
+import { ProtectedRoute } from '@/components/Login/ProtectedRoute'
 
 export default function App() {
 
@@ -11,7 +12,14 @@ export default function App() {
     <Flex className="App" h="100vh" direction="column">
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/main' element={<MainPage />} />
+        <Route 
+            path='/main' 
+            element={
+              <ProtectedRoute>
+                <MainPage />
+              </ProtectedRoute>
+            } 
+          />
       </Routes>
     </Flex>
     </BrowserRouter>
